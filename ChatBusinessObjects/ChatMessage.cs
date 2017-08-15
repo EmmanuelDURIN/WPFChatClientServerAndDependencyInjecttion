49bb7d5d4798d818715158586bd2863c8a84294d
Emmanuel Durin <emmanueldurin@gmail.com>
2017-08-15 17:40:40 +0200

    Création des PCL

---
+++ ChatBusinessObjects/ChatMessage.cs
@@ -0,0 +1,18 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ChatBusinessObjects
+{
+    public class ChatMessage : BindableBase
+    {
+        public int Id { get; set; }
+        public String Speaker { get; set; }
+        public DateTime EmissionDate { get; set; }
+        public String Content { get; set; }
+    }
+}
