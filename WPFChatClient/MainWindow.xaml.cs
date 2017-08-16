2c98a8d5f90885e2ebff97b4793b68816025106c
Emmanuel Durin <emmanueldurin@gmail.com>
2017-08-16 21:18:07 +0200

    Chargement de dummy data au run time

--- WPFChatClient/MainWindow.xaml.cs
+++ WPFChatClient/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ChatViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,12 @@ namespace WPFChatClient
   /// </summary>
   public partial class MainWindow : Window
   {
+    private MainWindowViewModel viewModel = new MainWindowViewModel();
+
     public MainWindow()
     {
       InitializeComponent();
+      DataContext = viewModel;
     }
   }
 }
