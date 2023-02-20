3f4d2fd9187b065b134c6ca63849dc920f000f43
Emmanuel Durin <emmanueldurin@gmail.com>
2023-02-20 18:18:50 +0100

    Reciblage .Net 4.8

--- WPFChatClient/MainWindow.xaml.cs
+++ WPFChatClient/MainWindow.xaml.cs
@@ -1,32 +1,19 @@
 ﻿using ChatViewModel;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPFChatClient
 {
-  /// <summary>
-  /// Logique d'interaction pour MainWindow.xaml
-  /// </summary>
-  public partial class MainWindow : Window
-  {
-    private MainWindowViewModel viewModel = new MainWindowViewModel();
-
-    public MainWindow()
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
     {
-      InitializeComponent();
-      DataContext = viewModel;
+        private MainWindowViewModel viewModel = new MainWindowViewModel();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+        }
     }
-  }
 }
