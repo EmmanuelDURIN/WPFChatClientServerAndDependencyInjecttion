09232da18ec961a220b77a8d0a86c81162b62f95
Emmanuel Durin <emmanueldurin@gmail.com>
2023-02-20 21:59:02 +0100

    Reciblage .Net 4.8

--- ChatBusinessObjects/User.cs
+++ ChatBusinessObjects/User.cs
@@ -1,39 +1,40 @@
 ﻿using Model;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ChatBusinessObjects
 {
-  public class User : BindableBase
-  {
-    private String name;
-    private String password;
+    public class User : BindableBase
+    {
+        private String name;
+        private String password;
 
-    public String Name
-    {
-      get { return name; }
-      set
-      {
-        SetProperty(ref name, value);
-      }
+        public String Name
+        {
+            get { return name; }
+            set
+            {
+                SetProperty(ref name, value);
+            }
+        }
+        public String Password
+        {
+            get { return password; }
+            set
+            {
+                SetProperty(ref password, value);
+            }
+        }
+        public override bool Equals(object obj)
+        {
+            var user = obj as User;
+            if (user == null)
+                return false;
+            return Name == user.Name;
+        }
+        public override int GetHashCode()
+        {
+            return 363513814 + EqualityComparer<string>.Default.GetHashCode(name);
+        }
     }
-    public String Password
-    {
-      get { return password; }
-      set
-      {
-        SetProperty(ref password, value);
-      }
-    }
-    public override bool Equals(object obj)
-    {
-      var user = obj as User;
-      if (user == null)
-        return false;
-      return Name == user.Name;
-    }
-  }
 }
