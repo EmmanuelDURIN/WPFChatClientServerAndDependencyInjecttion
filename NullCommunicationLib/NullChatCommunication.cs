9088042b0abf907f3c10fd3d6623c56f52d2f82f
Emmanuel Durin <emmanueldurin@gmail.com>
2023-02-20 21:36:30 +0100

    Reciblage .Net 4.8

--- NullCommunicationLib/NullChatCommunication.cs
+++ NullCommunicationLib/NullChatCommunication.cs
@@ -1,30 +1,27 @@
 ﻿using ChatBusinessObjects;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
 namespace ChatBusinessLogic
 {
-  public class NullChatCommunication : IChatCommunication
-  {
-    public event Action<ChatMessage> MessageReceived;
-    public async Task Connect(string userName, string password, CancellationToken token)
+    public class NullChatCommunication : IChatCommunication
     {
-      Task task = Task.Delay(2000, token);
-      await task;
+        public event Action<ChatMessage> MessageReceived;
+        public async Task Connect(string userName, string password, CancellationToken token)
+        {
+            Task task = Task.Delay(2000, token);
+            await task;
+        }
+        public async Task Disconnect(CancellationToken token)
+        {
+            Task task = Task.Delay(2000, token);
+            await task;
+        }
+        public async Task SendMessage(ChatMessage message)
+        {
+            Task task = Task.Delay(2000);
+            await task;
+        }
     }
-    public async Task Disconnect(CancellationToken token)
-    {
-      Task task = Task.Delay(2000, token);
-      await task;
-    }
-    public async Task SendMessage(ChatMessage message)
-    {
-      Task task = Task.Delay(2000);
-      await task;
-    }
-  }
 }
