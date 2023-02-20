92595d82e49c3290dafcf36a6bb87cd8d7f96737
Emmanuel Durin <emmanueldurin@gmail.com>
2023-02-20 18:51:54 +0100

    Reciblage .Net 4.8

--- WPFChatClient/MainWindow.xaml.cs
+++ WPFChatClient/MainWindow.xaml.cs
@@ -1,42 +1,29 @@
 ﻿using ChatViewModel;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WPFChatClient
 {
-  public partial class MainWindow : Window
-  {
-    [Import(nameof(MainWindowViewModel))]
-  //  [Import("MainWindowViewModel")]
-    private MainWindowViewModel viewModel = null;
+    public partial class MainWindow : Window
+    {
+        [Import(nameof(MainWindowViewModel))]
+        //  [Import("MainWindowViewModel")]
+        private MainWindowViewModel viewModel = null;
 
-    public MainWindow()
-    {
-      InitializeComponent();
-      CompositionContainer container = WPFContainer.Instance;
-      //Lazy<MainWindowViewModel>  lazyVM = container.GetExport<MainWindowViewModel>(nameof(MainWindowViewModel));
-      //viewModel = lazyVM.Value;
-      //viewModel = new MainWindowViewModel();
-      container.ComposeParts(this);
-      DataContext = viewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+            CompositionContainer container = WPFContainer.Instance;
+            //Lazy<MainWindowViewModel>  lazyVM = container.GetExport<MainWindowViewModel>(nameof(MainWindowViewModel));
+            //viewModel = lazyVM.Value;
+            //viewModel = new MainWindowViewModel();
+            container.ComposeParts(this);
+            DataContext = viewModel;
+        }
+        private void PasswordBoxPasswordChanged(object sender, RoutedEventArgs e)
+        {
+            viewModel.PasswordChanged(passwordBox.Password);
+        }
     }
-    private void PasswordBoxPasswordChanged(object sender, RoutedEventArgs e)
-    {
-      viewModel.PasswordChanged(passwordBox.Password);
-    }
-  }
 }
